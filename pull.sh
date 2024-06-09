1b975034a3b557a3ad2491b446232eecdcd6f648
renhai-lab <3152498252@qq.com>
2024-06-09 13:52:32 +0800

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,15 +1,12 @@
+#!/bin/sh
 # Enable debug mode
 set -x
 
 # 拉取镜像，指定 CPU 架构
-cat trigger.txt | awk '{print "docker pull --platform linux/arm64 " $1}'
 cat trigger.txt | awk '{print "docker pull --platform linux/arm64 " $1}' | sh
-
-cat trigger.txt | awk '{print "docker pull --platform linux/amd64 " $1}'
 cat trigger.txt | awk '{print "docker pull --platform linux/amd64 " $1}' | sh
 
 # 检查镜像架构信息
-cat trigger.txt | awk '{print "docker image inspect " $1 " | grep Architecture" }'
 cat trigger.txt | awk '{print "docker image inspect " $1 " | grep Architecture" }' | sh
 
 # 标记镜像
@@ -49,4 +46,10 @@ cat trigger.txt | while read -r line; do
 
   # 推送 arm64 架构的镜像
   arm64_tag="${dest_image}-arm64"
-  e
+  echo "Pushing $arm64_tag (arm64) to the registry..."
+  docker push $arm64_tag
+  if [ $? -ne 0 ]; then
+    echo "Failed to push $arm64_tag (arm64)"
+    exit 1
+  fi
+done

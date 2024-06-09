6aef4e59021a789f22b35c30d8778d067927a0e9
renhai-lab <3152498252@qq.com>
2024-06-09 13:08:37 +0800

    debug

--- pull.sh
+++ pull.sh
@@ -5,15 +5,26 @@ cat trigger.txt | while read -r line; do
   image=$(echo $line | awk '{print $1}')
   tag=$(echo $line | awk '{print $2}')
 
-  # Pull for arm64 and amd64 architectures separately
+  # Pull for both arm64 and amd64 architectures
   docker pull --platform linux/arm64 $image
   docker pull --platform linux/amd64 $image
 
+  # Tag images for the registry
+  docker tag $image $REGISTRY/$ALIYUN_HUB_NAME/$tag
+
+  # Push images to the registry
+  docker push $REGISTRY/$ALIYUN_HUB_NAME/$tag
+done
+
+# Create and push a manifest list for multi-architecture support
+cat trigger.txt | while read -r line; do
+  tag=$(echo $line | awk '{print $2}')
+
   # Create a manifest list for multi-architecture support
-  docker manifest create $REGISTRY/$tag \
-    --amend $image@$(docker image inspect --format='{{index .RepoDigests 0}}' $image --platform linux/arm64) \
-    --amend $image@$(docker image inspect --format='{{index .RepoDigests 0}}' $image --platform linux/amd64)
+  docker manifest create $REGISTRY/$ALIYUN_HUB_NAME/$tag \
+    --amend $REGISTRY/$ALIYUN_HUB_NAME/$tag:arm64 \
+    --amend $REGISTRY/$ALIYUN_HUB_NAME/$tag:amd64
 
   # Push the manifest list
-  docker manifest push $REGISTRY/$tag
+  docker manifest push $REGISTRY/$ALIYUN_HUB_NAME/$tag
 done

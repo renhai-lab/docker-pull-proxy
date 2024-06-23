377ac8408e126d1e7f22af85e31603c8e414f717
renhai-lab <3152498252@qq.com>
2024-06-23 16:38:44 +0800

    修改

--- replace_hub_name.sh
+++ replace_hub_name.sh
@@ -4,5 +4,3 @@ HUB_NAME="${ALIYUN_HUB_NAME}"
 # 替换 trigger.txt 中的 HUB_NAME_PLACEHOLDER
 sed -i "s|HUB_NAME_PLACEHOLDER|$HUB_NAME|g" trigger-amd64.txt
 sed -i "s|HUB_NAME_PLACEHOLDER|$HUB_NAME|g" trigger-arm64.txt
-# 输出替换后的文件内容以供调试
-cat trigger.txt
